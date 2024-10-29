9e39bd4732d3bf533af5db5b7c23dc9c1b07d9c6
Juan Cruz Masante <d47708394@instituto.itr.edu.ar>
2024-10-29 08:35:02 -0300

    anda

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-import mysql.connector
+from flask_migrate import Migrate  # Importa Flask-Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token
 
 app = Flask(__name__)
-app.secret_key = 'portafolio'
+app.secret_key = 'porta'
 
+# Configura la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ugk2nsgl77xe7w0j:qPSKJQALkY2eQP4gWzhf@bikibggurwnmevjbgtwu-mysql.services.clever-cloud.com:3306/bikibggurwnmevjbgtwu'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "porta"
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # Inicializa Flask-Migrate
+jwt = JWTManager(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
 def home():
@@ -58,3 +70,5 @@ def log_in():
 
     return render_template('log_in.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
