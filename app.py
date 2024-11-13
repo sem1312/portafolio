6d840f51ca2182f3f9a2d010f72ee69c681a7368
Pajan <d47708394@alumnos.itr.edu.ar>
2024-11-13 10:44:12 -0600

    casi

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+# app.py
+from flask import Flask, render_template, redirect, url_for, request, flash, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -12,7 +14,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ugk2nsgl77xe7w0j:qPSKJQALkY2eQP
 db = SQLAlchemy()
 db.init_app(app)
 
-class User(db.Model):
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
@@ -20,9 +25,12 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# Envuelve la creación de las tablas en un contexto de aplicación
 with app.app_context():
     db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -33,13 +41,32 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.check_password(password):
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
             login_user(user)
             return redirect(url_for('index'))
         else:
             flash('Invalid credentials')
     return render_template('login.html')
+
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created successfully')
+            return redirect(url_for('login'))
+    return render_template('sign_in.html')
+
+@app.route('/view_cv')
+def view_cv():
+    return send_file('static/pdf/CV.pdf', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
